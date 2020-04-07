0f3baffe8f05bdadf2e1496ea689e20e745378c0
Christophe <christophe.lamy.cl@gmail.com>
2020-04-07 16:33:11 +0200

    Calculette

---
+++ calculette.php
@@ -0,0 +1,2 @@
+<?php
+echo ($argv[1]+$argv[2]);
--- test.php
+++ test.php
@@ -1,5 +1,20 @@
 <?php
-error_reporting(E_ALL);
-$prenom1 = 'Quentin';
-$prenom2 = 'Aurore';
-echo('Hello' . $prenom . $prenom2);
+
+if ($argv[2] == "/") {
+	if ($argv[3] == "0")
+    	echo "pas possible";
+    		else echo ($argv[1]/$argv[3]);
+
+} elseif ($argv[2] == "-") {
+    echo ($argv[1]-$argv[3]);
+
+} elseif ($argv[2] == "+") {
+    echo ($argv[1]+$argv[3]);
+
+} elseif ($argv[2] == "x") {
+    echo ($argv[1]*$argv[3]);
+
+} else {
+    echo "KO";
+}
+?>
